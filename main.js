529de06ca55c4470119f01dd4f3cfc0a1d67528e
goveo <yuriybrusentsov@gmail.com>
2018-01-02 19:57:23 +0200

    added onload transition

---
+++ main.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    
+    $('#main').transition('scale')
+    $('#main').transition('scale')
+});
+
+$('#sidebar-button').click(function () {
+    // $('#main').transition('scale')
+    // $('#main').transition('scale')
+    // $('#sidebar').transition('slide right')
+});
