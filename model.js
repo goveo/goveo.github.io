77d84b1b74db2bc633bcb7c9ebfe04f8a5e78fdf
goveo <yuriybrusentsov@gmail.com>
2018-01-02 13:50:06 +0200

    fixed sidebar

--- model.js
+++ model.js
@@ -1,6 +1,24 @@
 var app = new Vue({
-	el: '#app',
-	data: {
-		show: false
-	}
+    el: '#app',
+    data: {
+        show: false,
+        windowHeight: 0,
+        sidebarHeight: 0
+    },
+    created() {
+        window.addEventListener('resize', this.handleResize);
+    },
+    methods: {
+        handleResize: _.throttle(function () {
+            console.log('Resized');
+            this.calculateSidebar();
+        }, 100),
+        calculateSidebar() {
+            app.sidebarHeight = $('#sidebar').innerHeight;
+            app.windowHeight = window.innerHeight;
+            console.log("sidebarHeight ", sidebarHeight);
+            console.log("windowHeight ", windowHeight);
+        }
+
+    }
 });
