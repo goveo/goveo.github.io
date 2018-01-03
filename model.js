0d24f6bd19b96402cdf94895b9ad9f036fc8cd12
goveo <yuriybrusentsov@gmail.com>
2018-01-03 15:47:12 +0200

    added nice transitions

--- model.js
+++ model.js
@@ -2,21 +2,39 @@ var app = new Vue({
     el: '#app',
     data: {
         show: false,
-        mainPageStyle: 'sixteen wide aligned column'
+        mainStyle: 'sixteen wide column',
+        sidebarStyle: 'zero wide column'
     },
     created() {
-        $('#main').transition('pulse');
+        // $('#main').transition('pulse');
     },
     methods: {
         changeSidebarState() {
             console.log('sidebar state changed');
-            $('#sidebar').transition('slide right')
-            $('#main').transition('pulse');
-            if (app.show == true) {
-                app.mainPageStyle = 'sixteen wide aligned column';
+
+            $('#sidebar').transition('fade right');
+            
+            if (app.show) {
+                
+                app.mainStyle = 'sixteen wide column';
+                app.sidebarStyle = 'zero wide column transition hidden';
             } else {
-                app.mainPageStyle = 'twelve wide aligned column';
+                app.mainStyle = 'twelve wide column';
+                app.sidebarStyle = 'four wide column';
             }
+            
+            // $('#main').toggleClass("sixteen wide column");
+            // $('#main').toggleClass("twelve wide column");
+            // $('#sidebar').toggleClass("zero wide column");
+            // $('#sidebar').toggleClass("four wide column");
+
+            // $('#sidebar').transition('slide right');
+            // $('#main').transition('pulse');
+            // if (app.show == true) {
+            //     app.mainPageStyle = 'sixteen wide aligned column';
+            // } else {
+            //     app.mainPageStyle = 'twelve wide aligned column';
+            // }
 
             app.show = !app.show;
         },
