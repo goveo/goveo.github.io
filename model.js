eac3af167e6b728e86c7d3c82481b8f67d5753d9
goveo <yuriybrusentsov@gmail.com>
2018-01-03 16:27:30 +0200

    fixed transition speed

--- model.js
+++ model.js
@@ -5,7 +5,7 @@ var app = new Vue({
         sidebarStyle: 'zero wide column'
     },
     created() {
-        // $('#main').transition('pulse');
+        $('#main').transition('pulse');
     },
     methods: {
         changeSidebarState() {
@@ -18,19 +18,6 @@ var app = new Vue({
             } else {
                 app.sidebarStyle = 'four wide column';
             }
-            
-            // $('#main').toggleClass("sixteen wide column");
-            // $('#main').toggleClass("twelve wide column");
-            // $('#sidebar').toggleClass("zero wide column");
-            // $('#sidebar').toggleClass("four wide column");
-
-            // $('#sidebar').transition('slide right');
-            // $('#main').transition('pulse');
-            // if (app.show == true) {
-            //     app.mainPageStyle = 'sixteen wide aligned column';
-            // } else {
-            //     app.mainPageStyle = 'twelve wide aligned column';
-            // }
 
             app.show = !app.show;
         },
